d1c91a6c4e36df473aa91c24be88106bf0bb2dae
Ravi Kishor <97280268+Ravi-Kishor@users.noreply.github.com>
2022-11-03 12:50:50 +0530

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -120,10 +120,10 @@ dl_uptodown() {
 
 patch_apk() {
 	local stock_input=$1 patched_apk=$2 patcher_args=$3
-	echo "java -jar $RV_CLI_JAR --rip-lib x86 --rip-lib x86_64 --rip-lib armeabi-v7a -c -a $stock_input -o $patched_apk -b $RV_PATCHES_JAR --keystore=ks.keystore $patcher_args"
+	echo "java -jar $RV_CLI_JAR --rip-lib x86 --rip-lib x86_64 -c -a $stock_input -o $patched_apk -b $RV_PATCHES_JAR --keystore=ks.keystore $patcher_args"
 	# shellcheck disable=SC2086
 	# --rip-lib is only available in my own revanced-cli builds
-	java -jar "$RV_CLI_JAR" --rip-lib x86 --rip-lib x86_64 -c -a "$stock_input" -o "$patched_apk" -b "$RV_PATCHES_JAR" --keystore=ks.keystore $patcher_args
+	java -jar "$RV_CLI_JAR" --rip-lib x86 --rip-lib x86_64 --rip-lib armeabi-v7a -c -a "$stock_input" -o "$patched_apk" -b "$RV_PATCHES_JAR" --keystore=ks.keystore $patcher_args
 }
 
 zip_module() {
